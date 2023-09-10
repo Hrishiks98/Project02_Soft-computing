73c63fbd03525bc3ebd8cb516569ae42a1eb4808
RISHABH SALUJA <143355227+Hrishiks98@users.noreply.github.com>
2023-09-10 18:03:22 +0530

    Add files via upload

---
+++ Norm.m
@@ -0,0 +1,6 @@
+function input_norm = normalize(z)
+input_norm=(z-min(z))/(max(z)-min(z));
+%UNTITLED6 Summary of this function goes here
+
+end
+
---
+++ tansigmoid_func.m
@@ -0,0 +1,4 @@
+function tf = transfer(ls)
+tf=(exp(ls)- exp(-ls))./(exp(ls)+ exp(-ls));
+end
+
